d660eec8395503ca6c0c5c3a4847ba0244221f48
rick <rfarouni@gmail.com>
2017-02-21 13:44:05 -0500

    vae model

--- VAEs/vae.py
+++ VAEs/vae.py
@@ -12,36 +12,36 @@ from keras.layers import Dense
 from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets("mnist", one_hot=True)
-n_samples = mnist.train.num_examples
+NUM_SAMPLES = mnist.train.num_examples
+BATCH_SIZE   = 100
+NUM_BATCHES = int(NUM_SAMPLES / BATCH_SIZE)
+INPUT_DIM    = 28 * 28
+HIDDEN_DIM   = 1024
+LATENT_DIM   = 8
+EPOCHS       = 120
 
-# MODEL
-
-M = 100  # batch size during training
-P = 28 * 28
-# parameters
-D = 2  # latent dimension
-L = 512 # number of hidden units
 
 def NN_p(z):
-  layer_1 = Dense(L, activation='relu')(z)
-  layer_2 = Dense(P)(layer_1)
+  layer_1 = Dense(HIDDEN_DIM, activation='relu')(z)
+  layer_2 = Dense(INPUT_DIM)(layer_1)
   return layer_2
 
-def NN_q(x_ph):
-  layer_1 = Dense(L, activation='relu')(x_ph)
-  layer_2 = Dense(L, activation='relu')(layer_1)
-  mu = Dense(D)(layer_2)
-  sigma = Dense(D, activation='softplus')(layer_2)
+def NN_q(x_q):
+  layer_1 = Dense(HIDDEN_DIM, activation='relu')(x_q)
+  layer_2 = Dense(HIDDEN_DIM, activation='relu')(layer_1)
+  mu = Dense(LATENT_DIM)(layer_2)
+  sigma = Dense(LATENT_DIM, activation='softplus')(layer_2)
   return mu, sigma
 
 # Generative model
-z = Normal(mu=tf.zeros([M, D]), sigma=tf.ones([M, D]))
+z = Normal(mu=tf.zeros([BATCH_SIZE, LATENT_DIM]),
+           sigma=tf.ones([BATCH_SIZE, LATENT_DIM]))
 logits = NN_p(z.value())
 x = Bernoulli(logits=logits)
 
 # Inference model
-x_ph = tf.placeholder(tf.float32, [M, P])
-mu, sigma = NN_q(x_ph)
+x_q = tf.placeholder(tf.float32, [BATCH_SIZE, INPUT_DIM])
+mu, sigma = NN_q(x_q)
 z_q = Normal(mu=mu, sigma=sigma)
 
 
@@ -50,7 +50,7 @@ sess = ed.get_session()
 K.set_session(sess)
 
 # Bind p(x, z) and q(z | x) to placeholder
-inference = ed.KLqp({z: z_q}, data={x: x_ph})
+inference = ed.KLqp({z: z_q}, data={x: x_q})
 optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
 inference.initialize(optimizer=optimizer)
 
@@ -59,14 +59,12 @@ init.run()
 
 # Run Model
 
-n_epoch = 100
-total_batch = int(n_samples / M)
-for epoch in range(n_epoch):
+for epoch in range(EPOCHS):
   avg_loss = 0.0
-  for t in range(total_batch):
-    x_train, _ = mnist.train.next_batch(M)
-    info_dict = inference.update(feed_dict={x_ph: x_train})
+  for t in range(NUM_BATCHES):
+    x_train, _ = mnist.train.next_batch(BATCH_SIZE)
+    info_dict = inference.update(feed_dict={x_q: x_train})
     avg_loss += info_dict['loss']
-  avg_loss = avg_loss / n_samples
+  avg_loss = avg_loss / NUM_SAMPLES
   print("log p(x) >= {:0.3f}".format(avg_loss))
 
